c154bf539a5eb34d294303c9fb059b65c08cf4ea
azinck94 <Andrew_zinck@student.uml.edu>
2017-06-27 16:09:20 -0400

    wrote the curl request for the signup.sh file wrote the curl request for the signup.sh file

---
+++ assets/scripts/signup.sh
@@ -0,0 +1,6 @@
+curl "https://aqueous-atoll-85096.herokuapp.com" \
+  --include \
+  --request POST \
+  --data-urlencode "credentials[email]=$EMAIL" \
+  --data-urlencode "credentials[password]=$PASSWORD" \
+  --data-urlencode "credentials[password_confirmation]=$PASSWORD"
